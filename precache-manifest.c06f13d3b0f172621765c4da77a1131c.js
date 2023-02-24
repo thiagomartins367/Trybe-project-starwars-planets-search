5a2a8803b4756859f3db5466363bf26e82e4e40c
thiagomartins367 <thiagomartins367@users.noreply.github.com>
2023-02-24 12:33:39 +0000

    deploy: 371a7dd198693617a34626a4da2e50bfba861de8

---
+++ precache-manifest.c06f13d3b0f172621765c4da77a1131c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78af30509bf9ae2e901e51f883b18efb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "856f16dd53ad1f7c60a6",
+    "url": "/static/css/main.72f58f68.chunk.css"
+  },
+  {
+    "revision": "502ceebbcdd438e17883",
+    "url": "/static/js/2.ed5ccc99.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ed5ccc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856f16dd53ad1f7c60a6",
+    "url": "/static/js/main.d0e6b8be.chunk.js"
+  },
+  {
+    "revision": "3d22c4ff75fc8bca25f7",
+    "url": "/static/js/runtime-main.68c0a999.js"
+  }
+]);
